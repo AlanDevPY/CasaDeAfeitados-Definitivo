a0473773b01d00153f56a08f46f58ea1928b1e0b
Alan Romero <emanuromet@gmail.com>
2023-11-02 15:32:18 -0300

    add code

---
+++ Javascrip/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDPL-stOdAU3LBkSR5gI2-sKdzVMO0cjGo",
+    authDomain: "casadeafeitados-2135b.firebaseapp.com",
+    projectId: "casadeafeitados-2135b",
+    storageBucket: "casadeafeitados-2135b.appspot.com",
+    messagingSenderId: "794942479230",
+    appId: "1:794942479230:web:53d8bfc4daa711807bf12f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
